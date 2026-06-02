2184bb21c443b29eb430e1d26a5f5a2f9e16cef3
NotTimTam <timothyrp6@gmail.com>
2026-06-01 20:27:11 -0700

    Add padding to header

--- src/ui/header.py
+++ src/ui/header.py
@@ -5,16 +5,16 @@ class Header:
 
     def __init__(self, title="Home"):
         self.title = title
+        self.padding = 4
 
     def get_height(self, draw, font):
         bbox = draw.textbbox((0, 0), self.title, font=font)
-        return bbox[3] - bbox[1]
+        return (bbox[3] - bbox[1]) + self.padding
 
     def render(self, img, draw, font):
         bbox = draw.textbbox((0, 0), self.title, font=font)
         line_height = bbox[3] - bbox[1]
-        padding = 4
-        line_height += padding
+        line_height += self.padding
 
         draw.text((int((SCREEN_SIZE / 2) - (bbox[2] / 2)), 0), self.title, fill=(255, 255, 255), font=font)
-        draw.line((0, line_height, SCREEN_SIZE, line_height), fill=(255, 255, 255), width=1)
+        draw.line((0, line_height + self.padding, SCREEN_SIZE, line_height + self.padding), fill=(255, 255, 255), width=1)
